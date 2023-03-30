9556909e1ad54a9cce55a24a40ae0d57fbe90f1b
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-03-30 14:13:19 -0500

    Unit Testing 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPolindrome(string word)
+		{
+			var reverse = "";
+			for (int i = word.Length - 1; i >= 0; i--)
+			{
+				reverse += word[i];
+			}
+			if (reverse. ToLower() == word.ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void MyTest(string word, bool expected)
         {
+            //arrange
+            var tester = new WordSmith();
 
+            //act
+            var actual = tester.IsAPolindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
+
     }
 }
